cc5e5278785d1a8152cf1b764722f20a6e7c1dc3
atdejimmyg <jamesgilljames9@gmail.com>
2019-11-14 17:33:45 +0000

    Added underground camera controls

---
+++ Assets/Scripts/cameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class cameraControl : MonoBehaviour
+{
+    public Camera OvergroundCamera;
+    public Camera UndergroundCamera;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            UndergroundCamera.enabled = true;
+            OvergroundCamera.enabled = false;
+        }
+        else
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                UndergroundCamera.enabled = false;
+                OvergroundCamera.enabled = true;
+            }
+
+        }
+
+    }
+}
