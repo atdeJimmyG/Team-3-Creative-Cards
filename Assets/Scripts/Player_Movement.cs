fa100335895a8266d929256cbf0e3bb8991ab2a7
atdeJimmyG <jamesgilljames9@gmail.com>
2019-11-21 00:28:51 +0000

    added animations

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,26 +6,73 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    float defaultSpeed = 1.5f;
-    float runSpeed = 4;
+    float defaultSpeed = 2.5f;
+    float runSpeed = 4.5f;
     [SerializeField]
     float speed = 1.5f;
     Vector3 moveVector;
+    public Animator animator;
+    private bool m_FacingRight = true;
+
     void FixedUpdate()
     {
         transform.position += moveVector * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = runSpeed;
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isWalking", false);
+
         }
         else
         {
             speed = defaultSpeed;
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
         }
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) <= 0.1)
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isIdle", false);
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) 
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isIdle", false);
+        }
+
+        
+
 
     }
     void Update()
     {
         moveVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed;
+
+        if (Input.GetAxis("Horizontal") > 0.1 && !m_FacingRight)
+        {
+            flip();
+        }
+        if (Input.GetAxis("Horizontal") < 0.1 && m_FacingRight)
+        {
+            flip();
+        }
+        
+    }
+
+    private void flip()
+    {
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        
     }
 }
