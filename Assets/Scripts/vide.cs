dbe92db4a1bac2c96e1a8639fed6dd9555345c0e
Jammes <jamesgilljames9@gmail.com>
2019-11-22 10:37:24 +0000

    added video as splash screen and fow

---
+++ Assets/Scripts/vide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vide : MonoBehaviour
+{
+    MovieTexture myMovie;
+
+
+    void Start()
+    {
+        myMovie.Play();
+    }
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), myMovie);
+    }
+}
